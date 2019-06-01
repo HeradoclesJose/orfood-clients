86a88c246dfcc7119366de73ddfdad7c37258291
Freddy Montoro <freddymontoro@gmail.com>
2019-06-01 23:35:13 +0000

    Pool and Keep Alive

--- Server/db/dbconnection.js
+++ Server/db/dbconnection.js
@@ -22,16 +22,20 @@ module.exports = {
       'password':dbinfo.db.mysqlPassword,
       'database':dbinfo.db.mysqlDatabase,
       'host':dbinfo.db.mysqlHost,
-      'port':dbinfo.db.mysqlPort
+      'port':dbinfo.db.mysqlPort,
+      'connectionLimit': 10
     }
 
-    const connection = mysql.createConnection(options)
-    connection.connect(err => {
+    const connection = mysql.createPool(options)
+    connection.getConnection(err => {
       if (err) {
         console.error('Error connecting to Mysql Database')
         throw err
       }
     })
+
+   
+
     console.log('Connected to WordpressDB')
     return connection;
 
