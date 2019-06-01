cd48c84c9156b9b89c86942800061b211dfe7ee6
Unknown <heradocles@gmail.com>
2019-06-01 13:16:40 -0400

    CORSS
    Self exp

--- Server/server.js
+++ Server/server.js
@@ -39,8 +39,9 @@ app.use(bodyParser.urlencoded({ limit: '15mb', extended: true, parameterLimit: 5
 // Allow CROSS-ORIGINS
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, HEAD, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
   next()
 })
 
